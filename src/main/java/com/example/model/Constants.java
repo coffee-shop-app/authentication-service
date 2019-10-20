51629408e4d816754633878ea98bf58dbf151fa3
abhijitk7 <abhikulkarni.1988@gmail.com>
2019-10-20 20:53:10 +0530

    Removed user name and password

--- src/main/java/com/example/model/Constants.java
+++ src/main/java/com/example/model/Constants.java
@@ -3,7 +3,7 @@ package com.example.model;
 public class Constants {
 
     public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5*60*60;
-    public static final String SIGNING_KEY = "devglan123r";
+    public static final String SIGNING_KEY = "ad165b11320bc91501ab08613cc3a48a62a6caca4d5c8b14ca82cc313b3b96cd";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String AUTHORITIES_KEY = "scopes";
